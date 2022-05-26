628dc1089a081ba8e3dc8bed3021c7115370d9ef
robin <83653446+perperam@users.noreply.github.com>
2022-05-26 13:29:44 +0200

    Created the yourtils reop for my Progressbar

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from progessbar import *
---
+++ progressbar.py
@@ -0,0 +1,55 @@
+class Progressbar():
+    def __init__(self, to_value, name="", prefix="Progress:", suffix="Complete"):
+        self.to_value = to_value
+        self.is_value = 0
+        self.name = name
+        self.prefix = prefix
+        self.suffix = suffix
+        self.form = "\r{prefix} [{bar}] {is_value}/{to_value} {name} {suffix} "
+        self.print_bar()
+
+    def print_bar(self):
+        BAR_SIZE = 30
+        test_value = lambda isv : 1 if (isv > 0) else 0
+        equal = round(BAR_SIZE * self.is_value/self.to_value) -1
+        under = BAR_SIZE - equal
+
+        if self.is_value == 0:
+            equal = 0
+            under = BAR_SIZE
+        if self.is_value == self.to_value:
+            under = 0
+            equal = BAR_SIZE
+
+        bar = "=" * equal + ">" * test_value(self.is_value/self.to_value) + "_" * under
+
+        print(self.form.format(
+            bar=bar,
+            prefix = self.prefix,
+            name = self.name,
+            suffix = self.suffix,
+            is_value=self.is_value,
+            to_value=self.to_value,
+            ), end="\r")
+        if self.is_value == self.to_value:
+            print()
+
+    def add(self, value):
+        self.is_value += value
+        self.print_bar()
+
+    def set(self, value):
+        self.is_value = value
+        self.print_bar()
+
+
+def example():
+    from time import sleep
+    amount = 10
+    Pbar = Progressbar(amount+1)
+    for i in range(amount*2):
+        Pbar.add(0.5)
+        sleep(0.2)
+
+if __name__ == "__main__":
+    example()
